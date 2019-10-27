1d42ccd3445ecc3f342469a626cf070dbbe2d5cb
Ibou <idiallo@gwu.edu>
2019-10-27 19:55:31 -0400

    portfolio commit

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class JobsModel(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
+
+
+class BlogModel(models.Model):
+    pass
