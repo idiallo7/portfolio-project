b2379c9f449448482a252ff4eba0db355e1bc130
Ibou <idiallo@gwu.edu>
2019-11-17 12:30:34 -0500

    Settings change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD':'@posDakar7',
+        'PASSWORD':'@posTest7',
         'HOST': 'localhost',
         'PORT': '5432',
     }
